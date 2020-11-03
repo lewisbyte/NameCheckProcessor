e8e0c130df07765819f2e84dd6af0fa8282cce5b
lewis <lingyidx@gmail.com>
2020-11-03 21:09:12 +0800

    init project

---
+++ src/main/java/com/qq1367212627/github/NameCheckProcessor.java
@@ -0,0 +1,45 @@
+package com.qq1367212627.github;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+/**
+ *
+ * @author lewis
+ *
+ */
+
+@SupportedAnnotationTypes("*")
+@SupportedSourceVersion(SourceVersion.RELEASE_6)
+public class NameCheckProcessor extends AbstractProcessor {
+
+
+    /**
+     * Initializes the processor with the processing environment by
+     * setting the {@code processingEnv} field to the value of the
+     * {@code processingEnv} argument.  An {@code
+     * IllegalStateException} will be thrown if this method is called
+     * more than once on the same object.
+     *
+     * @param processingEnv environment to access facilities the tool framework
+     *                      provides to the processor
+     * @throws IllegalStateException if this method is called more than once.
+     */
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param annotations
+     * @param roundEnv
+     */
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
