3c6455a414515fe9be093843a94efea25d110b80
lewis <lingyidx@gmail.com>
2020-11-05 00:00:16 +0800

    ADD: 完善代码

---
+++ src/main/java/com/lewisbyte/example/BAD_CODE.java
@@ -0,0 +1,12 @@
+package com.lewisbyte.example;
+
+public class BAD_CODE {
+    public static final String aaa="111";
+
+    public static final int ___aa= 1;
+
+    public static void BAD_CODE(String[] args) {
+        return;
+    }
+
+}
